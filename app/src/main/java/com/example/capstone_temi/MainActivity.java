28fd6f80693ce40841cdba6ce53e9a561dbddcc6
Jin Yang Ng <s10203386@connect.np.edu.sg>
2022-10-17 15:48:50 +0800

    Implemented package functions

--- app/src/main/java/com/example/capstone_temi/MainActivity.java
+++ app/src/main/java/com/example/capstone_temi/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.capstone_temi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.robotemi.sdk.NlpResult;
 import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.TtsRequest;
+import com.robotemi.sdk.activitystream.ActivityStreamPublishMessage;
 import com.robotemi.sdk.listeners.OnBeWithMeStatusChangedListener;
 import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener;
 import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements
+        Robot.NlpListener,
+        OnRobotReadyListener,
+        Robot.ConversationViewAttachesListener,
+        Robot.WakeupWordListener,
+        Robot.ActivityStreamPublishListener,
+        Robot.TtsListener,
+        OnBeWithMeStatusChangedListener,
+        OnGoToLocationStatusChangedListener,
+        OnLocationsUpdatedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +34,48 @@ public class MainActivity extends AppCompatActivity {
         //test chen-han branch
     }
 
+    @Override
+    public void onPublish(@NonNull ActivityStreamPublishMessage activityStreamPublishMessage) {
+
+    }
+
+    @Override
+    public void onConversationAttaches(boolean b) {
+
+    }
+
+    @Override
+    public void onNlpCompleted(@NonNull NlpResult nlpResult) {
+
+    }
+
+    @Override
+    public void onTtsStatusChanged(@NonNull TtsRequest ttsRequest) {
+
+    }
+
+    @Override
+    public void onWakeupWord(@NonNull String s, int i) {
+
+    }
+
+    @Override
+    public void onBeWithMeStatusChanged(@NonNull String s) {
+
+    }
+
+    @Override
+    public void onGoToLocationStatusChanged(@NonNull String s, @NonNull String s1, int i, @NonNull String s2) {
+
+    }
+
+    @Override
+    public void onLocationsUpdated(@NonNull List<String> list) {
+
+    }
+
+    @Override
+    public void onRobotReady(boolean b) {
+
+    }
 }
