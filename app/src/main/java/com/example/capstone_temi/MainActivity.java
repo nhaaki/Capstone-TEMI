16a51822880021fd95e80c169b1b3421b0ea656f
Jin Yang Ng <s10203386@connect.np.edu.sg>
2022-10-18 14:35:00 +0800

    removed sdk for awhile

--- app/src/main/java/com/example/capstone_temi/MainActivity.java
+++ app/src/main/java/com/example/capstone_temi/MainActivity.java
@@ -3,79 +3,86 @@ package com.example.capstone_temi;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.robotemi.sdk.NlpResult;
-import com.robotemi.sdk.Robot;
-import com.robotemi.sdk.TtsRequest;
-import com.robotemi.sdk.activitystream.ActivityStreamPublishMessage;
-import com.robotemi.sdk.listeners.OnBeWithMeStatusChangedListener;
-import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener;
-import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
-import com.robotemi.sdk.listeners.OnRobotReadyListener;
+//import com.robotemi.sdk.NlpResult;
+//import com.robotemi.sdk.Robot;
+//import com.robotemi.sdk.TtsRequest;
+//import com.robotemi.sdk.activitystream.ActivityStreamPublishMessage;
+//import com.robotemi.sdk.listeners.OnBeWithMeStatusChangedListener;
+//import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener;
+//import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
+//import com.robotemi.sdk.listeners.OnRobotReadyListener;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements
-        Robot.NlpListener,
-        OnRobotReadyListener,
-        Robot.ConversationViewAttachesListener,
-        Robot.WakeupWordListener,
-        Robot.ActivityStreamPublishListener,
-        Robot.TtsListener,
-        OnBeWithMeStatusChangedListener,
-        OnGoToLocationStatusChangedListener,
-        OnLocationsUpdatedListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        Button button = findViewById(R.id.button);
 
-    @Override
-    public void onPublish(@NonNull ActivityStreamPublishMessage activityStreamPublishMessage) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, BotDirectingPage.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
 
-    }
 
-    @Override
-    public void onConversationAttaches(boolean b) {
 
-    }
-
-    @Override
-    public void onNlpCompleted(@NonNull NlpResult nlpResult) {
 
     }
 
-    @Override
-    public void onTtsStatusChanged(@NonNull TtsRequest ttsRequest) {
-
-    }
-
-    @Override
-    public void onWakeupWord(@NonNull String s, int i) {
-
-    }
-
-    @Override
-    public void onBeWithMeStatusChanged(@NonNull String s) {
-
-    }
-
-    @Override
-    public void onGoToLocationStatusChanged(@NonNull String s, @NonNull String s1, int i, @NonNull String s2) {
-
-    }
-
-    @Override
-    public void onLocationsUpdated(@NonNull List<String> list) {
-
-    }
-
-    @Override
-    public void onRobotReady(boolean b) {
-
-    }
+//    @Override
+//    public void onPublish(@NonNull ActivityStreamPublishMessage activityStreamPublishMessage) {
+//
+//    }
+//
+//    @Override
+//    public void onConversationAttaches(boolean b) {
+//
+//    }
+//
+//    @Override
+//    public void onNlpCompleted(@NonNull NlpResult nlpResult) {
+//
+//    }
+//
+//    @Override
+//    public void onTtsStatusChanged(@NonNull TtsRequest ttsRequest) {
+//
+//    }
+//
+//    @Override
+//    public void onWakeupWord(@NonNull String s, int i) {
+//
+//    }
+//
+//    @Override
+//    public void onBeWithMeStatusChanged(@NonNull String s) {
+//
+//    }
+//
+//    @Override
+//    public void onGoToLocationStatusChanged(@NonNull String s, @NonNull String s1, int i, @NonNull String s2) {
+//
+//    }
+//
+//    @Override
+//    public void onLocationsUpdated(@NonNull List<String> list) {
+//
+//    }
+//
+//    @Override
+//    public void onRobotReady(boolean b) {
+//
+//    }
 }
