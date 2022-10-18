d9d05740f040a348e751212a6abaac01ff7bda4f
Chen Han <s10202961@connect.np.edu.sg>
2022-10-18 13:46:16 +0800

    Added button to the main page this navigates to the second page where bot will be directing the user

--- app/src/main/java/com/example/capstone_temi/MainActivity.java
+++ app/src/main/java/com/example/capstone_temi/MainActivity.java
@@ -31,8 +31,7 @@ public class MainActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test chen-han
-        //test hakimi
+
     }
 
     @Override
