5257816dbc9a5439379a677d1eef500b89d67b61
Chen Han <73086331+Chen-Han-NP@users.noreply.github.com>
2022-12-29 16:27:38 +0800

    Added camera button

--- app/src/main/java/com/example/capstone_temi/MainActivity.java
+++ app/src/main/java/com/example/capstone_temi/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.capstone_temi;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,17 +17,31 @@ public class MainActivity extends AppCompatActivity {
     private WebView webView = null;
     public String url = "https://chen-han-np.github.io/Capstone-TEMI-Website-Demo/";
 
+    public Button takePhotoButton;
+    public Button sendBut;
+    public ImageView imageSending;
+
+    public Button reload;
+    public Button back;
+
+    private static final int CAMERA_PIC_REQUEST = 1337;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.activity_main);
 
-        Button reload = findViewById(R.id.refresh);
-        Button back = findViewById(R.id.back);
+        // For level 2 feature showcase
+        takePhotoButton = (Button) findViewById(R.id.takePhotoBut);
+        sendBut = (Button) findViewById(R.id.sendBut);
+        imageSending = (ImageView) findViewById(R.id.personImage);
 
+        reload = findViewById(R.id.refresh);
+        back = findViewById(R.id.back);
 
         this.webView = (WebView) findViewById(R.id.webView);
 
@@ -35,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(webViewClient);
         webView.loadUrl(url);
 
+        // For level 2 showcase
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
 
 
         reload.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +76,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imageSending.setImageBitmap(image);
+        }
+    }
+
 
 
 
