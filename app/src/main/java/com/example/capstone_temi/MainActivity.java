26db071d5fd96d11cff9296f149b7173b178a774
JinYana <73157602+JinYana@users.noreply.github.com>
2022-12-30 10:20:55 +0800

    made changes to UI

--- app/src/main/java/com/example/capstone_temi/MainActivity.java
+++ app/src/main/java/com/example/capstone_temi/MainActivity.java
@@ -3,16 +3,23 @@ package com.example.capstone_temi;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.face.ContactModel;
+import com.robotemi.sdk.face.OnFaceRecognizedListener;
+import com.robotemi.sdk.listeners.OnMovementStatusChangedListener;
 
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public Button takePhotoButton;
     public Button sendBut;
     public ImageView imageSending;
-
+    public TextView name;
     public Button reload;
     public Button back;
 
@@ -40,7 +47,20 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
+
+        // For level 2 feature showcase
+        takePhotoButton = (Button) findViewById(R.id.takePhotoBut);
+        sendBut = (Button) findViewById(R.id.sendBut);
+        imageSending = (ImageView) findViewById(R.id.personImage);
+
+        reload = (Button) findViewById(R.id.refresh);
+        back = (Button) findViewById(R.id.back);
+
         robot = Robot.getInstance();
+
+        name = findViewById(R.id.name);
+
+
 
 
         Button dance = findViewById(R.id.dance);
@@ -48,13 +68,35 @@ public class MainActivity extends AppCompatActivity {
         dance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Boolean dancing = true;
                 robot.turnBy(360, 1);
+                robot.turnBy(-360, 1);
+
+                robot.tiltBy(-25, 1);
+                robot.tiltBy(55, 1);
+
+
+
+                robot.addOnMovementStatusChangedListener(new OnMovementStatusChangedListener() {
+                    @Override
+                    public void onMovementStatusChanged(@NonNull String type, @NonNull String status) {
+
+                        int times = 0;
+
+                        if(type.equals("turnBy")){
+                            if(status.equals("complete")){
+                                times += 1;
+                                if(times == 2){
+                                    robot.stopMovement();
+                                }
+                            }
+                        }
+
+                    }
+                });
             }
         });
-        // For level 2 feature showcase
-        takePhotoButton = (Button) findViewById(R.id.takePhotoBut);
-        sendBut = (Button) findViewById(R.id.sendBut);
-        imageSending = (ImageView) findViewById(R.id.personImage);
+
 
 
         this.webView = (WebView) findViewById(R.id.webView);
@@ -89,6 +131,33 @@ public class MainActivity extends AppCompatActivity {
                 webView.goBack();
             }
         });
+
+        Button face = findViewById(R.id.face);
+
+        robot.addOnFaceRecognizedListener(new OnFaceRecognizedListener() {
+            @Override
+            public void onFaceRecognized(@NonNull List<ContactModel> list) {
+
+                Log.v("urmum", "suck");
+
+
+                name.setText(list.get(0).getFirstName() + " " + list.get(0).getLastName());
+                robot.stopFaceRecognition();
+
+            }
+        });
+
+        face.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.v("urmum", "jkdn");
+            }
+        });
+
+
+
+
     }
 
 
@@ -100,7 +169,6 @@ public class MainActivity extends AppCompatActivity {
             imageSending.setImageBitmap(image);
         }
     }
-
 
 
 
