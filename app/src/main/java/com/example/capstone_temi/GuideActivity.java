ca3a96f1f112f4f5fd446c5d1e5d987920b35c5a
Chen Han <73086331+Chen-Han-NP@users.noreply.github.com>
2023-01-03 15:56:44 +0800

    Edited level 2 feature

--- app/src/main/java/com/example/capstone_temi/GuideActivity.java
+++ app/src/main/java/com/example/capstone_temi/GuideActivity.java
@@ -1,11 +1,14 @@
 package com.example.capstone_temi;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -16,7 +19,12 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
@@ -39,6 +47,7 @@ import com.robotemi.sdk.map.MapDataModel;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
@@ -69,6 +78,7 @@ public class GuideActivity extends AppCompatActivity implements
     public String bookId; // Bookid from the req URL
     public String bookName; // BookName from the req URL
     public Robot robot;
+    public Bitmap imageReceived;
     public Boolean answer = true;
 
 
@@ -159,6 +169,57 @@ public class GuideActivity extends AppCompatActivity implements
 
             // For different Level TEMIs
             else{
+                // Launch take photo
+                ActivityResultLauncher<Intent> imageActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                        new ActivityResultCallback<ActivityResult>() {
+                            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                            @Override
+                            public void onActivityResult(ActivityResult result) {
+                                if (result.getResultCode() == Activity.RESULT_OK) {
+                                    Intent data = result.getData();
+                                    imageReceived = (Bitmap) data.getExtras().get("data");
+                                    if (imageReceived != null) {
+                                        // Send the image in json
+                                        String requestUrl = goserver + "/image";
+                                        JSONObject postData = new JSONObject();
+
+                                        // Encode the bitmap
+                                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                        imageReceived.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                                        byte[] imageBytes = baos.toByteArray();
+                                        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+                                        try {
+                                            postData.put("image", encodedImage);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, requestUrl, postData, new Response.Listener<JSONObject>() {
+                                            @Override
+                                            public void onResponse(JSONObject response) {
+                                            }
+                                        }, new Response.ErrorListener() {
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+                                                error.printStackTrace();
+                                            }
+                                        });
+
+                                        RequestQueue nameRequestQueue = Volley.newRequestQueue(GuideActivity.this);
+                                        nameRequestQueue.add(jsonObjectRequest);
+                                    }
+                                }
+                            }
+                        });
+
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                imageActivityResultLauncher.launch(intent);
+
+
+
+
+
+
                 String requestUrl = goserver + "/wronglevel";
                 JSONObject postData = new JSONObject();
                 try {
